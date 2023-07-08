711502d6328233ae4329ec2850997731195bb93b
Mstriato <misaelstriato@outlook.com>
2023-07-08 16:34:51 -0300

    Variaveis

---
+++ tipos.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
+    <title>Tipos</title>
+</head>
+<body>
+    <div>
+        <?php
+        $nome = "Misael";
+        $peso = 89;
+        echo $nome . " quer ter " . $peso . "Kg <br/>";
+        echo (string) $nome . "<br/>";
+        echo (int) $nome;
+        ?>
+    </div>
+    
+</body>
+</html>
