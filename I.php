5fff0346e6316cbfa4adfd5afe509d04bd5753ec
Mstriato <misaelstriato@outlook.com>
2023-07-10 21:54:07 -0300

    Integrações

---
+++ I.php
@@ -0,0 +1,17 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de Integração</title>
+</head>
+<body>
+    <?php 
+    $rq = sqrt($_GET["a"]);
+    echo $rq;
+    ?>
+
+    
+</body>
+</html>
