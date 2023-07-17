f40ffdf44fcd51947ca781e015b206284768d078
Mstriato <misaelstriato@outlook.com>
2023-07-16 21:00:54 -0300

    Update do_while2.php

--- do_while2.php
+++ do_while2.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="_css/estilo.css">
     <title>Document</title>
 </head>
 <body>
+<div>
     <?php 
     $num = $_GET["num"];
     $fat = 1;
@@ -16,6 +18,7 @@
         $c--;
     } while ($c>=1);
     ?>
-    <a href="index/012.html">Voltar</a>
+    <a class="botao" href="index/012.html">Voltar</a>
+</div>
 </body>
 </html>
